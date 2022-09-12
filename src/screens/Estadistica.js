d41894fdaead5b43cd16193dddc75dabf75c91f8
xP4ST3x <81394640+xP4ST3x@users.noreply.github.com>
2022-09-12 09:08:24 -0300

    Update Estadistica.js

--- src/screens/Estadistica.js
+++ src/screens/Estadistica.js
@@ -1,4 +1,7 @@
+import Personas from "../components/Personas"
 export default function Estadistica() {
+    let menores = []
+    let mayores = []
     return (
         <>
             <p>Estadistica</p>
