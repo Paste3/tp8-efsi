cfe30f986edaf7fbcca6c8a8677f19669f1f9fd1
xP4ST3x <81394640+xP4ST3x@users.noreply.github.com>
2022-09-13 08:46:48 -0300

    Update Estadistica.js

--- src/screens/Estadistica.js
+++ src/screens/Estadistica.js
@@ -1,21 +1,31 @@
 import Personas from "../components/Personas"
 export default function Estadistica() {
-    var mayores = [];
     let mayores35 = [];
-    for(let i = 0;i<Personas.length;i++){
-        if(Personas[i].edad > 35){
-            mayores35.push(Personas[i].nombre);
+    let mayorEdad = [0];
+    let menorEdad = [0];
+    for (let i = 0; i < Personas.length; i++) {
+        if (Personas[i].edad >= 35) {
+            mayores35.push(Personas[i]);
         }
-        if(Personas[i].edad == mayores[i]){
-            mayores.push(Personas[i].edad);
-        }
-        else{
-            mayores[i] = Personas[i].edad;
+        for (let g = 0; g < mayorEdad[g]; g++) {
+            if (Personas[i].edad > mayorEdad[g]) {
+                mayorEdad.push(Personas[i].edad)
+            }
+            if (Personas[i].edad < menorEdad[g]) {
+                menorEdad.push(Personas[i].edad)
+            }
         }
     }
     return (
         <>
-            <p>Estadistica</p>
+            <p>Personas mayores a 35 años({mayores35.length + 1}):</p>
+            <ul>
+                {
+                    mayores35.map(mayor => (
+                        <li> {mayor.nombre} </li>
+                    ))
+                }
+            </ul>
         </>
-        )
+    )
 }
